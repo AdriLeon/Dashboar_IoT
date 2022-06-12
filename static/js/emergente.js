985254cabc5877399c708fc18535b2537f06a681
AdriLeon <51243138+AdriLeon@users.noreply.github.com>
2022-06-12 04:55:57 -0500

    setup.html terminada

---
+++ static/js/emergente.js
@@ -0,0 +1,14 @@
+(function ($) {
+  "use strict";
+
+  var $accountDelete = $("#delete-account"),
+    $accountDeleteDialog = $("#confirm-delete");
+
+  $accountDelete.on("click", function () {
+    $accountDeleteDialog[0].showModal();
+  });
+
+  $("#cancel").on("click", function () {
+    $accountDeleteDialog[0].close();
+  });
+})(jQuery);
