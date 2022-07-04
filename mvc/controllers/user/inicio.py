2d3f4a9e7d21b371befccad0bd5e6a9b54275c8b
AdriLeon <51243138+AdriLeon@users.noreply.github.com>
2022-07-04 10:09:14 -0500

    creacion de ventanas emergentes, modificacion de javascript (no funciona al ejecutan con python)

--- mvc/controllers/user/inicio.py
+++ mvc/controllers/user/inicio.py
@@ -3,6 +3,7 @@ import pyrebase
 import firebase_config as token 
 import app as app
 import json
+import random
 
 render = web.template.render("mvc/views/user/")
 
@@ -25,3 +26,25 @@ class Inicio:
                 return render.inicio(name, url)
         except Exception as error:
             print("Error Inicio.GET: {}".format(error))
+    def POST(self):
+        try:
+            firebase = pyrebase.initialize_app(token.firebaseConfig)
+            db = firebase.database()
+            formulario = web.input()
+            widget = formulario.widget
+            type = formulario.type
+            if type == "humedad":
+                b = random.radint(10000000,20000000)
+                wid = hex(b)
+                localId = web.cookies().get('localId')
+                data = {
+                    "widget": widget,
+                    "temperatura": None,
+                    "humedad": None
+                }
+                db.child("users").child(localId).child("data_widget").child(wid).set(data)
+                return web.seeother("/inicio")
+            else:
+                return web.seeother("/inicio")
+        except Exception as error:
+            print("Error Inicio.POST: {}".format(error))
--- static/js/emergente.js
+++ static/js/emergente.js
@@ -1,14 +1,65 @@
 (function ($) {
   "use strict";
 
-  var $accountDelete = $("#delete-account"),
-    $accountDeleteDialog = $("#confirm-delete");
+  var $accountDelete = $("#delete-account"), //boton para abrir
+    $accountDeleteDialog = $("#confirm-delete"), //ventana
+    $distancebutton = $("#distance-button"),
+    $distanceemergent = $("#distance-emergent"),
+    $speedbutton = $("#speed-button"),
+    $speedemergent = $("#speed-emergent"),
+    $humeditybutton = $("#humedity-button"),
+    $humedityemergent = $("#humedity-emergent"),
+    $soundbutton = $("#sound-button"),
+    $soundemergent = $("#sound-emergent"),
+    $powerbutton = $("#power-button"),
+    $poweremergent = $("#power-emergent");
 
   $accountDelete.on("click", function () {
     $accountDeleteDialog[0].showModal();
   });
 
-  $("#cancel").on("click", function () {
+  $distancebutton.on("click", function () {
+    $distanceemergent[0].showModal();
+  });
+
+  $speedbutton.on("click", function () {
+    $speedemergent[0].showModal();
+  });
+
+  $humeditybutton.on("click", function () {
+    $humedityemergent[0].showModal();
+  });
+
+  $soundbutton.on("click", function () {
+    $soundemergent[0].showModal();
+  });
+
+  $powerbutton.on("click", function () {
+    $poweremergent[0].showModal();
+  });
+
+  $("#cancel").on("click", function () { //boton cancelar
     $accountDeleteDialog[0].close();
   });
+
+  $("#cancel1").on("click", function () { //boton cancelar
+    $distanceemergent[0].close();
+  });
+
+  $("#cancel2").on("click", function () { //boton cancelar
+    $humedityemergent[0].close();
+  });
+
+  $("#cancel3").on("click", function () { //boton cancelar
+    $speedemergent[0].close();
+  });
+
+  $("#cancel4").on("click", function () { //boton cancelar
+    $soundemergent[0].close();
+  });
+
+  $("#cancel5").on("click", function () { //boton cancelar
+    $poweremergent[0].close();
+  });
+
 })(jQuery);
